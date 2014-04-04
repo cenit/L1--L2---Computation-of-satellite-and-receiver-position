017dbe2a6e1134b307a5607445650cffd763f018
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-04 15:25:29 +0200

    Added steps 8-11
    unsure on step 8 and 11.  Don’t know where T_A_to_s and I_A_to_s
    correction equations are.

--- matlab_files/Lab1.m
+++ matlab_files/Lab1.m
@@ -7,13 +7,13 @@ mu = 3.986005e14; % universal gravitational parameter (m/s)^3
 omega_e_dot = 7.2921151467e-5; % earth rotation rate (rad/s)
 F = -4.442807633e-10; % s/m^1/2
 %% Compute signal propagation time by (13)
-P1 = 23640467.92143; 
+P1 = 23640467.92143;
 ta_nom = seconds_in_week(1,1,14,0); % 1 hour and 14 minutes
 tAtoS = P1/c; % signal propagation time
 %% Compute signal transmission time by (14)
 ts_nom = ta_nom - tAtoS;
-%% Compute satellite clock correction ?tsL1 
-% by (24) and (25), neglect ?tr
+%% Compute satellite clock correction dtsL1
+% by (24) and (25), neglect dtr
 af0 = -3.137718886140D-05;
 af1 = 2.273736754430D-13;
 af2 = 0.000000000000D+00;
@@ -36,9 +36,9 @@ tk = ts - toe;
 tk = fixTk(tk); % if,then for table 2 of tk
 mk = m0 + n*tk;
 Ek = keplersEquation(mk,ec);
-%% Compute ?tr by (26) and ts by (15).
+%% Compute dtr by (26) and ts by (15).
 dtr = F*ec*sqrt(A)*sin(Ek); %(26)
-ts_new = ts - dtr;
+ts_with_dtr = ts - dtr;
 %% Compute satellite coordinates Xs, Ys, Zs, for time ts - Table 2
 % Calculate rk
 crs = -9.471875000000D+01;
@@ -71,3 +71,32 @@ ykp = rk*sin(uk);
 xk = xkp*cos(omegak) - ykp*cos(ik)*sin(omegak);
 yk = xkp*sin(omegak) + ykp*cos(ik)*cos(omegak);
 zk = ykp*sin(ik);
+Xs = xk;
+Ys = yk;
+Zs = zk;
+%% Compute satellite clock correction dtsL1 by (24) - (27)
+dtsL1_with_dtr = dtsL1 + dtr; % (24)
+%% 9. Compute tropospheric correction T_A_to_s (tA)
+%% 10. Compute ionospheric correction I_A_to_s (tA)
+%% 11. Compute approximate distance rho_A0_to_s (tA) by (11).
+dts = 0; % terms with dts are negligible, so I set it to zero
+XA0 = 0; % 
+YA0 = 0; % approximate receiver coordinates
+ZA0 = 0; %
+rho_A0_to_s = sqrt(...
+    (Xs - XA0 + omega_e_dot*YA0*dts)^2 + ... % x^2
+    (Ys - YA0 + omega_e_dot*XA0*dts)^2 + ... % y^2
+    (Zs - ZA0)^2   ... % z^2
+);
+%% 12. Repeat steps 1 - 11 for all measured satellites.
+%% 13. Compute elements of vector L (19).
+%% 14. Compute elements of matrix A (20); a_x_to_s , a_y_to_s , a_z_to_s by (12)
+%% 15. Estimate unknown parameters by (18)
+%% 16. Update receiver coordinates by (22)
+%% 17. Repeat steps 11 -16 until the solution has converged. 
+% The solution has converged if the following condition is
+% fulfilled: (vTv)i −(vTv)i−1 <ε, where ε is a small number
+% and depends on the numerical accuracy, ε = 1e-5 should 
+% suffice to preserve mm numerical precision of the computed
+% coordinates; i is iteration number. The vector v is computed
+% after step 13 by Equation (17).
