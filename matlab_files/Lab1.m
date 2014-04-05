3c8e2db4b4b5fdb3aceb328d1cabe9dc7a55ab93
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-05 18:17:08 +0200

    L and A added
    last file before function to calculate all numbers for satellites

--- matlab_files/Lab1.m
+++ matlab_files/Lab1.m
@@ -7,14 +7,20 @@ c = 299792458; % speed of light (m/s)
 mu = 3.986005e14; % universal gravitational parameter (m/s)^3
 omega_e_dot = 7.2921151467e-5; % earth rotation rate (rad/s)
 F = -4.442807633e-10; % s/m^1/2
+%% Import P1 numbers and satellite numbers
+% observation file 
+p1_numbers = importObsP1numbers('0lov033b.04o', 1371, 1392); % Doesn't change
+satelliteNumbers = importObsSatelliteNumbers('0lov033b.04o', 1370, 1370); % Doesn't change
+[XA0,YA0,ZA0] = importApproxPosition('0lov033b.04o',8, 8); % Doesnt change
 %% Import navigation file
 % includes parameters
 navfiles = importNavigationFiles('0lov033b.04n');
+% Now it becomes satelite specific
 i = 1:8:112;
 sat = navfiles(i(5):i(5)+8,:);
 sat = transpose(cell2mat(sat));
 sat = num2cell(sat);
-% Imports numbers to all variables
+% Imports numbers to all variables ... changes per satellite
 [satnumber,     af0,        af1,        af2,...
     iode,       crs,        dn,         m0,...
     cuc,        ec,         cus,        sqrtA,...
@@ -23,14 +29,9 @@ sat = num2cell(sat);
     idot,       ~,          ~,          ~,...
     ~,          ~,          tgd,        ~]...
     =sat{:};
-%% Import P1 numbers and satellite numbers
-% observation file 
-p1_numbers = importObsP1numbers('0lov033b.04o', 1371, 1392);
-satelliteNumbers = importObsSatelliteNumbers('0lov033b.04o', 1370, 1370);
-[XA0,YA0,ZA0] = importApproxPosition('0lov033b.04o',8, 8);
 %% Compute signal propagation time by (13)
-P1 = p1_numbers(1);
-ta_nom = seconds_in_week(1,1,14,0); % 1 hour and 14 minutes
+P1 = p1_numbers(1); % specific per time and satellite
+ta_nom = seconds_in_week(1,1,14,0); % 1 hour and 14 minutes My Time
 tAtoS = P1/c; % signal propagation time
 %% Compute signal transmission time by (14)
 ts_nom = ta_nom - tAtoS;
@@ -93,7 +94,9 @@ rho_A0_to_s = sqrt(...
 % rho_A_to_s = P1 + c*dtsL1_with_dtr - c*dtA; % (8) dtA =\= 0
 %% 12. Repeat steps 1 - 11 for all measured satellites.
 %% 13. Compute elements of vector L (19).
+Lmatrix = P1 - rho_A0_to_s + c*dtsL1_with_dtr;
 %% 14. Compute elements of matrix A (20); a_x_to_s , a_y_to_s , a_z_to_s by (12)
+Amatrix = 1/rho_A0_to_s*[(Xs - XA0),(Ys - YA0),(Zs - ZA0)];
 %% 15. Estimate unknown parameters by (18)
 %% 16. Update receiver coordinates by (22)
 %% 17. Repeat steps 11 -16 until the solution has converged.
