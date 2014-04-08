0603807b8709698c5bf3052a492fe07c1e6da013
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-08 19:12:29 +0200

    Revert 01a466e..37b1df7
    This rolls back to commit 01a466e596a4f73e3f0b45c56f6ab7f34a3c4ddb.

--- matlab_files/Lab1.m
+++ matlab_files/Lab1.m
@@ -1,6 +1,5 @@
 %% Computation of receiver's position
 addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position')
-addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position/matlab_files/')
 cd('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position')
 clear all;
 clc;
@@ -13,38 +12,42 @@ F = -4.442807633e-10; % s/m^1/2
 p1_numbers = importObsP1numbers('0lov033b.04o', 1371, 1392); % Doesn't change
 satelliteNumbers = importObsSatelliteNumbers('0lov033b.04o', 1370, 1370); % Doesn't change
 [XA0,YA0,ZA0] = importApproxPosition('0lov033b.04o',8, 8); % Doesnt change
-%% Initialize variables
-% rho = zeros(length(satelliteNumbers))
 %% Import navigation file
 % includes parameters
 navfiles = importNavigationFiles('0lov033b.04n');
-%% Now it becomes time specific
-% Steps 1->11 loop
-numberOfSat = 11;
-for satelliteCount = 1:numberOfSat
-    [ rho(satelliteCount,:),...
-        Xs(satelliteCount,:),...
-        Ys(satelliteCount,:),...
-        Zs(satelliteCount,:),...
-        dtsL1_with_dtr(satelliteCount,:)] = satLandP( satelliteCount,p1_numbers(satelliteCount),navfiles,XA0,YA0,ZA0 );
+% Now it becomes satelite specific
+for satelliteNumberOrder = 1:11
+    [ Lmat(satelliteNumberOrder,:), ...
+        Amat(satelliteNumberOrder,:),...
+        rho_A0_to_s(satelliteNumberOrder,:),...
+        Xs(satelliteNumberOrder,:),Ys(satelliteNumberOrder,:),...
+        Zs(satelliteNumberOrder,:),P1(satelliteNumberOrder,:),...
+        dtsL1_with_dtr(satelliteNumberOrder,:)]...
+        = satLandP( satelliteNumberOrder,p1_numbers(satelliteNumberOrder),navfiles,XA0,YA0,ZA0 );
 end
-%% Repetition of 11-16
-% for i = 1:2 % iterations for receiver coordinate estimates
+for i = 1:11 % iterations for receiver coordinate estimates
+    %% 11. Compute approximate distance rho_A0_to_s (tA) by (11).
+    dts = 0; % terms with dts are negligible, so I set it to zero
+    rho_A0_to_s = sqrt(...
+        (Xs - XA0 + omega_e_dot*YA0*dts).^2 + ... % x^2
+        (Ys - YA0 + omega_e_dot*XA0*dts).^2 + ... % y^2
+        (Zs - ZA0).^2   ... % z^2
+        );
+    %% 12. Repeat steps 1 - 11 for all measured satellites.
     %% 13. Compute elements of vector L (19).
-    Lmat = p1_numbers - rho + c*dtsL1_with_dtr;
+    Lmatrix = P1 - rho_A0_to_s + c*dtsL1_with_dtr;
     %% 14. Compute elements of matrix A (20); a_x_to_s , a_y_to_s , a_z_to_s by (12)
-    Afake = [(Xs - XA0),(Ys - YA0),(Zs - ZA0),ones(length(Xs),1)];
-    %%
-    unknownX = (Amat'*Amat)\(Amat'*Lmat);
-    %%
-    newXaYaZa = [XA0;YA0;ZA0] + unknownX(1:3,i);
+    Amatrix = 1/rho_A0_to_s*[(Xs - XA0),(Ys - YA0),(Zs - ZA0),rho_A0_to_s];
+    
+    %% 15. Estimate unknown parameters by (18)
+    unknownParameterX(:,i) = (Amat'*Amat)\(Amat'*Lmat);
+    %% 16. Update receiver coordinates by (22)
+    newXaYaZa = [XA0,YA0,ZA0]' + unknownParameterX(1:3,i);
     newXYZcell = num2cell(newXaYaZa);
     [XA0,YA0,ZA0] = newXYZcell{:};
-    v(:,i) = - Amat*unknownX(:,i) + Lmat;
-% end
-%% 15. Estimate unknown parameters by (18)
-% unknownParameterX1 = (Amat'*Amat)\(Amat'*Lmat);
-%% 16. Update receiver coordinates by (22)
+    v(:,i) = -Amat*unknownParameterX(:,i) + Lmat;
+    
+end
 %% 17. Repeat steps 11 -16 until the solution has converged.
 % The solution has converged if the following condition is
 % fulfilled: (vTv)i −(vTv)i−1 <ε, where ε is a small number
