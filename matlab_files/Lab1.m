68e59275ab76459dd83656b49cbb5ee6c4a3a4df
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-13 18:54:41 +0200

    Revised for handing in code

--- matlab_files/Lab1.m
+++ matlab_files/Lab1.m
@@ -1,43 +1,37 @@
 %% Computation of receiver's position
-addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position')
-addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position/matlab_files/')
-cd('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L1, L2 - Computation of satellite and receiver position')
 clear all;
-clc;
 c = 299792458; % speed of light (m/s)
 mu = 3.986005e14; % universal gravitational parameter (m/s)^3
 omega_e_dot = 7.2921151467e-5; % earth rotation rate (rad/s)
 F = -4.442807633e-10; % s/m^1/2
-time = [2,1,1,0]; % days, hours, minutes, seconds
+time = [2,1,14,0]; % days, hours, minutes, seconds
 junk = num2cell(time);
 [nday,nhours,nminutes,nseconds] = junk{:};
 clear junk;
+%% Import observer file
 lov033b = importObserverFileAsString('0lov033b.04o', 1, 5629);
-%% Import P1 numbers and satellite numbers
-% observation file
-[rowInObs,nOfRows] = findTimeInObsFunction( lov033b,time );
-p1_numbers = importObsP1numbers('0lov033b.04o', rowInObs+1,rowInObs+nOfRows*2); % Doesn't change
-satelliteNumbers = importObsSatelliteNumbers('0lov033b.04o', rowInObs,rowInObs); % Doesn't change
-[XA0,YA0,ZA0] = sampleFunction(lov033b);
-approxPos = [XA0,YA0,ZA0];
-%% Import navigation file, same for every time
-% includes parameters
+% Import P1 numbers and satellite numbers
+[rowInObs,nOfRows] = findTimeInObsFunction( lov033b,time ); % match your time with observer time
+p1_numbers = importObsP1numbers('0lov033b.04o', rowInObs+1,rowInObs+nOfRows*2); % Import P1 numbers from the matched time above
+satelliteNumbers = importObsSatelliteNumbers('0lov033b.04o', rowInObs,rowInObs); % Import satellite numbers from matched time
+[XA0,YA0,ZA0] = sampleFunction(lov033b); % Record Approximate Position
+%% Import navigation file
 navfiles = importNavigationFiles('0lov033b.04n');
-%% Match up satellite number with row in data
+%% Match satellite numbers with available satellites
 satNumMatch = navfiles(1:8:96,1); % Order of satellite numbers import
 sortedSatelliteNumbers = sortrows([satelliteNumbers',p1_numbers],1);
+%% Main loop steps 1-14
+% Calculates variables needed for correction iterations
 count = 1;
 for i = 1:length(satNumMatch)
-    %% Steps 1-11 done inside
+    %% Steps 1-14 done inside
     if cell2mat(satNumMatch(i))==sortedSatelliteNumbers(count,1)
         [ Lmat(count,:), ...
             Amat(count,:),...
             rho(count,:),...
-            Xs(count,:),Ys(count,:),...
-            Zs(count,:),P1(count,:),...
+            Xs(count,:),Ys(count,:),Zs(count,:),...
+            P1(count,:),...
             dtsL1_with_dtr(count,:),...
-            change_tsv(count,:),...
-            ts(count,:),...
             tAtoS(count,:)]...
             = satLandP( i,sortedSatelliteNumbers(count,2),navfiles,XA0,YA0,ZA0,nday,nhours,nminutes,nseconds);
         count = count + 1;
@@ -47,33 +41,27 @@ for i = 1:length(satNumMatch)
 end
 
 %% %% 17. Repeat steps 11 -16 until the solution has converged.
-% The solution has converged if the following condition is
-% fulfilled: (vTv)i −(vTv)i−1 <ε, where ε is a small number
-% and depends on the numerical accuracy, ε = 1e-5 should
-% suffice to preserve mm numerical precision of the computed
-% coordinates; i is iteration number. The vector v is computed
-% after step 13 by Equation (17).
+% The solution has converged if the condition is fulfilled
 for i = 1:10
-    changeX = (Amat'*Amat)\(Amat'*Lmat);
-    v(:,i) = -Amat*changeX + Lmat;
-    newXYZ = [XA0,YA0,ZA0] + changeX(1:3)';
+    changeX = (Amat'*Amat)\(Amat'*Lmat); % eq. (21)
+    v(:,i) = -Amat*changeX + Lmat; % eq. (17)
+    newXYZ = [XA0,YA0,ZA0] + changeX(1:3)'; % eq. (22) estimated coordinates 
     newxyzcell = num2cell(newXYZ);
     [XA0,YA0,ZA0] = newxyzcell{:};
     clear newxyzcell;
-    v(:,i) = -Amat*changeX + Lmat;
-    rho = sqrt(...
+    rho = sqrt(... % recompute rho
         (Xs - XA0 + omega_e_dot * YA0 * tAtoS).^2 + ... % x^2
         (Ys - YA0 - omega_e_dot * XA0 * tAtoS).^2 + ... % y^2
         (Zs - ZA0).^2   ... % z^2
         );
-    Amat = [-(Xs - XA0)./rho,...
+    Amat = [-(Xs - XA0)./rho,... % recompute matrix A
         -(Ys-YA0)./rho,...
         -(Zs-ZA0)./rho,...
         rho./rho];
-    Lmat = P1 - rho + c*dtsL1_with_dtr;
+    Lmat = P1 - rho + c*dtsL1_with_dtr; % recompute matrix L
     %% 17. Convergence condition
-    if i>1
-        condition = abs(v(:,end)'*v(:,end)-v(:,end-1)'*v(:,end-1));
+    if i>1 % check for convergence condition
+        condition = abs(v(:,end)'*v(:,end)-v(:,end-1)'*v(:,end-1)); % condition must be 1e-5 or lower
         if condition < 1e-4
             fprintf('Convergence condition met = %d\n',condition);
             %% Lab 2
